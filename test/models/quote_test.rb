5dbfd6ae5d910dd6def078d3360746392cabd43c
Isiah Singh <isiahsingh18@gmail.com>
2015-04-05 22:35:22 +0000

    Adding unique tag

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   test "unique_tag" do
+     quote = Quote.create(:saying => "The 90's was awesome", :author => 'Isiah Singh')
+     puts quote.inspect
+     expected = 'IS#' + quote.id.to_s
+     actual = quote.unique_tag
+     assert_equal(expected, actual)
    end
 end
